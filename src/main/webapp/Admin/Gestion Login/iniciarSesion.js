da76ded0f7d3fe6d6136ba09323416e271728fde
jhoni <jhoni@MateBookD16>
2023-07-20 16:19:33 -0500

    Ortografia, Json(Medicamentos)

--- src/main/webapp/Admin/Gestion Login/iniciarSesion.js
+++ src/main/webapp/Admin/Gestion Login/iniciarSesion.js
@@ -1,14 +1,17 @@
 function validar() {
-      const username = document.getElementById('usuario').value;
-      const password = document.getElementById('contraseña').value;
+            const username = document.getElementById('usuario').value;
+            const password = document.getElementById('contraseña').value;
 
-      if (username === 'admin' && password === 'admin') {
-        // Redireccionar a la página deseada
-        window.location.href = '../index.html';
-        return false; // Prevenir el envío normal del formulario
-      } else {
-        // Mostrar mensaje de error o realizar alguna otra acción
-        Swal.fire('Contraseña incorrecta', 'Intente de nuevo', 'error');
-        return false; // Prevenir el envío normal del formulario
-      }
-    }
+            if (username === 'admin' && password === 'admin') {
+                // Redireccionar a la página deseada
+                window.location.href = '../index.html';
+                return false; // Prevenir el envío normal del formulario
+            } else {
+                Swal.fire({
+                    icon: 'warning',
+                    text: 'Contraseña incorrecta, Intente de nuevo!',
+                    button: "Aceptar"
+                });
+                return false; // Prevenir el envío normal del formulario
+            }
+        }
